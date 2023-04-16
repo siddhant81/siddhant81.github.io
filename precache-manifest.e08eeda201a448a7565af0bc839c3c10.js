8f0a2a7a4febdc28dcc03d4f66e2f6d1b93fd057
Siddhant Gautam <siddhant2219@gmail.com>
2023-04-16 00:45:06 -0700

    Updates

---
+++ precache-manifest.e08eeda201a448a7565af0bc839c3c10.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "5a706c8c40dc1b50ced4",
+    "url": "/static/css/main.89b1c551.chunk.css"
+  },
+  {
+    "revision": "5a706c8c40dc1b50ced4",
+    "url": "/static/js/main.5a706c8c.chunk.js"
+  },
+  {
+    "revision": "c199c0ff3120a1f0a665",
+    "url": "/static/js/1.c199c0ff.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "207b1645a9a031723c241253e6450f2b",
+    "url": "/static/media/wanderer.207b1645.png"
+  },
+  {
+    "revision": "898c0407d74c75fa110de76293d96ddd",
+    "url": "/static/media/linkedin.898c0407.png"
+  },
+  {
+    "revision": "894ecd08fd32d0a072f6200bea9890bb",
+    "url": "/static/media/email.894ecd08.png"
+  },
+  {
+    "revision": "7f98fc58b87de5bca4606c7c24c124fd",
+    "url": "/index.html"
+  }
+];
